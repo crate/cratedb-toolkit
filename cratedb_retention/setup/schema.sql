e0374fc6851e509ca5bef0421f141f5a9af83b63
Andreas Motl <andreas.motl@crate.io>
2023-06-29 08:07:32 +0200

    [docs] Improve inline documentation of `schema.sql`

--- cratedb_retention/setup/schema.sql
+++ cratedb_retention/setup/schema.sql
@@ -7,12 +7,19 @@ CREATE TABLE IF NOT EXISTS {policy_table.fullname} (
     "partition_column" TEXT NOT NULL,           -- The source table column name used for partitioning.
 
     -- Retention parameters.
-    "retention_period" INTEGER NOT NULL,        -- Retention period in days.
-                                                -- TODO: Provide better description what this means.
+    "retention_period" INTEGER NOT NULL,        -- Retention period in days. The number of days data gets
+                                                -- retained before applying the retention policy.
 
     -- Target: Where data is moved/relocated to.
-    "reallocation_attribute_name" TEXT,         -- TODO: Describe the meaning of this column value.
-    "reallocation_attribute_value" TEXT,        -- TODO: Describe the meaning of this column value.
+
+    -- Targeting specific nodes.
+    -- You may want to designate dedicated nodes to be responsible for hot or cold storage types.
+    -- To do that, you can assign attributes to specific nodes, effectively tagging them.
+    -- https://crate.io/docs/crate/reference/en/latest/config/node.html#custom-attributes
+    "reallocation_attribute_name" TEXT,         -- Name of the node-specific custom attribute.
+    "reallocation_attribute_value" TEXT,        -- Value of the node-specific custom attribute.
+
+    -- Targeting an S3 repository.
     "target_repository_name" TEXT,              -- The name of a repository created with `CREATE REPOSITORY ...`.
 
     -- Strategy to apply for data retention.
