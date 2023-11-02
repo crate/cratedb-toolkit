d6b7f50f9b0585e44df5edfadd12c78fc019feb5
Andreas Motl <andreas.motl@crate.io>
2023-11-02 03:27:23 +0100

    cli: Add `ctk {cluster,shell,list-jobs}` subcommands

--- cratedb_toolkit/cli.py
+++ cratedb_toolkit/cli.py
@@ -3,7 +3,11 @@ from click_aliases import ClickAliasedGroup
 
 from cratedb_toolkit.util.cli import boot_click
 
+from .cluster.cli import cli as cloud_cli
 from .io.cli import cli as io_cli
+from .job.cli import cli_list_jobs
+from .shell.cli import cli as shell_cli
+
 
 @click.group(cls=ClickAliasedGroup)  # type: ignore[arg-type]
 @click.option("--verbose", is_flag=True, required=False, help="Turn on logging")
@@ -12,4 +16,9 @@ from .io.cli import cli as io_cli
 @click.pass_context
 def cli(ctx: click.Context, verbose: bool, debug: bool):
     return boot_click(ctx, verbose, debug)
+
+
+cli.add_command(cloud_cli, name="cluster")
 cli.add_command(io_cli, name="load")
+cli.add_command(shell_cli, name="shell")
+cli.add_command(cli_list_jobs)
--- cratedb_toolkit/job/croud.py
+++ cratedb_toolkit/job/croud.py
@@ -1,7 +1,7 @@
 from cratedb_toolkit.util.croud import CroudCall, CroudWrapper
 
 
-def jobs_list(cratedb_cloud_cluster_id: str):
+def jobs_list(cratedb_cloud_cluster_id: str, output_format: str = None, decode_output: bool = True):
     from croud.clusters.commands import import_jobs_list
     from croud.parser import Argument
 
@@ -13,5 +13,5 @@ def jobs_list(cratedb_cloud_cluster_id: str):
         ],
     )
 
-    wr = CroudWrapper(call)
+    wr = CroudWrapper(call, output_format=output_format, decode_output=decode_output)
     return wr.invoke()
