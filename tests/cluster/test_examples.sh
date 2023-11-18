b0ce547e1b8c40745ad8b3dd520a1c77c61817ac
Andreas Motl <andreas.motl@crate.io>
2023-11-18 02:08:43 +0100

    Cloud API: Improve testing

--- tests/cluster/test_examples.sh
+++ tests/cluster/test_examples.sh
@@ -9,6 +9,10 @@ test_cloud_cluster() {
   source examples/shell/cloud_cluster.sh
 }
 
+test_cloud_import() {
+  source examples/shell/cloud_import.sh
+}
+
 # Load shUnit2.
 HERE="$(dirname "$(realpath "$0")")"
 . ${HERE}/../util/shunit2
--- tests/cluster/test_examples_python.py
+++ tests/cluster/test_examples_python.py
@@ -1,6 +1,5 @@
 # Copyright (c) 2023, Crate.io Inc.
 # Distributed under the terms of the AGPLv3 license, see LICENSE.
-import subprocess
 from pathlib import Path
 
 import pytest
@@ -8,6 +7,9 @@ import responses
 from pytest_notebook.nb_regression import NBRegressionFixture
 
 import cratedb_toolkit
+
+pytestmark = pytest.mark.python
+
 
 ROOT = Path(__file__).parent.parent.parent
 
@@ -34,44 +36,6 @@ def test_example_cloud_cluster_exists_python(mocker, mock_cloud_cluster_exists):
     from examples.python.cloud_cluster import main
 
     main()
-
-
-def test_example_cloud_cluster_exists_shell(mocker, mock_cloud_cluster_exists, capfd):
-    """
-    Verify that the program `examples/shell/cloud_cluster.sh` works.
-    In this case, the `ctk` command is mocked completely, so this is nothing serious.
-    """
-
-    cratedb_toolkit.configure(
-        settings_accept_env=True,
-    )
-
-    mocker.patch.dict(
-        "os.environ",
-        {
-            "CRATEDB_CLOUD_SUBSCRIPTION_ID": "f33a2f55-17d1-4f21-8130-b6595d7c52db",
-            "CRATEDB_CLOUD_CLUSTER_NAME": "testcluster",
-            "CRATEDB_USERNAME": "crate",
-        },
-    )
-
-    script = ROOT / "tests" / "cluster" / "test_examples.sh"
-    subprocess.check_call(["bash", script])  # noqa: S603, S607
-
-    out, err = capfd.readouterr()
-
-    assert "cluster start" in out
-    assert "shell --command" in out
-
-    """
-    assert "Mont Blanc massif" in out
-    assert "Monte Rosa Alps" in out
-    assert "Deploying/starting/resuming CrateDB Cloud Cluster: id=None, name=plotz" in err
-    assert "Cluster information: name=plotz, url=https://plotz.aks1.westeurope.azure.cratedb.net:4200" in err
-    assert "Successfully acquired cluster" in err
-    assert "CONNECT OK" in err
-    assert "SELECT 2 rows in set (0.000 sec)" in err
-    """
 
 
 @responses.activate
