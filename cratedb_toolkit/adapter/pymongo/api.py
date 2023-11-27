76c205f7a453ec54eaadf4bef3ab2a122abe0835
Andreas Motl <andreas.motl@crate.io>
2023-11-27 12:31:07 +0100

    MongoDB/PyMongo: Add querying capabilities using JessiQL
    With corresponding improvements, the amalgamated PyMongo driver can now
    run 95% of the MongoDB "getting started" tutorial successfully.

--- cratedb_toolkit/adapter/pymongo/api.py
+++ cratedb_toolkit/adapter/pymongo/api.py
@@ -3,7 +3,9 @@ from unittest.mock import patch
 import pymongo.collection
 
 from cratedb_toolkit.adapter.pymongo.collection import collection_factory
+from cratedb_toolkit.sqlalchemy.patch import patch_types_map
 from cratedb_toolkit.util import DatabaseAdapter
+from cratedb_toolkit.util.pandas import patch_pandas_sqltable_with_extended_mapping
 
 
 class PyMongoCrateDbAdapter:
@@ -15,20 +17,30 @@ class PyMongoCrateDbAdapter:
         self.cratedb = DatabaseAdapter(dburi=dburi)
         self.collection_backup = pymongo.collection.Collection
 
+        collection_patched = collection_factory(cratedb=self.cratedb)  # type: ignore[misc]
+        self.patches = [
+            # Patch PyMongo's `Collection` implementation.
+            patch("pymongo.collection.Collection", collection_patched),
+            patch("pymongo.database.Collection", collection_patched),
+            # Converge a few low-level functions of PyMongo to no-ops.
+            patch("pymongo.mongo_client.MongoClient._ensure_session"),
+            patch("pymongo.mongo_client._ClientConnectionRetryable._get_server"),
+        ]
+
     def start(self):
-        self.__enter__()
+        self.adjust_sqlalchemy()
+        self.activate()
+
+    def stop(self):
+        self.deactivate()
 
     def __enter__(self):
-        self.configure_sqlalchemy()
-        self.activate_pymongo_adapter()
+        self.start()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        """
-        Restore patched functions.
-        """
-        pymongo.collection.Collection = pymongo.database.Collection = self.collection_backup  # type: ignore[misc]
+        self.stop()
 
-    def configure_sqlalchemy(self):
+    def adjust_sqlalchemy(self):
         """
         Configure CrateDB SQLAlchemy dialect.
 
@@ -38,25 +50,28 @@ class PyMongoCrateDbAdapter:
 
         https://cratedb.com/docs/crate/reference/en/latest/general/ddl/column-policy.html#dynamic
         """
+        # 1. Patch data types for CrateDB dialect.
+        # TODO: Upstream to `sqlalchemy-cratedb`.
+        patch_types_map()
+
+        # 2. Prepare pandas.
         # TODO: Provide unpatching hook.
-        from cratedb_toolkit.util.pandas import patch_pandas_io_sqldatabase_with_dialect_parameters
+        # TODO: Use `with table_kwargs(...)`.
+        from cratedb_toolkit.util.pandas import patch_pandas_sqltable_with_dialect_parameters
 
-        patch_pandas_io_sqldatabase_with_dialect_parameters(table_kwargs={"crate_column_policy": "'dynamic'"})
+        patch_pandas_sqltable_with_dialect_parameters(table_kwargs={"crate_column_policy": "'dynamic'"})
+        patch_pandas_sqltable_with_extended_mapping()
 
-    def activate_pymongo_adapter(self):
+    def activate(self):
         """
-        Swap in the MongoDB -> CrateDB adapter.
+        Swap in the MongoDB -> CrateDB adapter, by patching functions in PyMongo.
         """
-        self.patch_pymongo_noops()
-        pymongo.collection.Collection = pymongo.database.Collection = collection_factory(cratedb=self.cratedb)  # type: ignore[misc]
-
-    def patch_pymongo_noops(self):
-        """
-        Converge a few low-level functions of PyMongo to no-ops.
-        """
-        patches = [
-            patch("pymongo.mongo_client.MongoClient._ensure_session"),
-            patch("pymongo.mongo_client._ClientConnectionRetryable._get_server"),
-        ]
-        for patch_ in patches:
+        for patch_ in self.patches:
             patch_.start()
+
+    def deactivate(self):
+        """
+        Swap out the MongoDB -> CrateDB adapter, by restoring patched functions.
+        """
+        for patch_ in self.patches:
+            patch_.stop()
--- cratedb_toolkit/util/pandas.py
+++ cratedb_toolkit/util/pandas.py
@@ -1,9 +1,9 @@
 import typing as t
 
-from pandas.io.sql import SQLTable
+import sqlalchemy as sa
 
 
-def patch_pandas_io_sqldatabase_with_dialect_parameters(table_kwargs: t.Dict = None):
+def patch_pandas_sqltable_with_dialect_parameters(table_kwargs: t.Dict = None):
     """
     When using pandas' `to_sql` function, configure the SQLAlchemy database dialect
     implementation using custom dialect parameters.
@@ -15,6 +15,9 @@ def patch_pandas_io_sqldatabase_with_dialect_parameters(table_kwargs: t.Dict = N
     table_kwargs = table_kwargs or {}
 
     # Provide enhancement code.
+    # It needs to be inlined in order to obtain `table_kwargs`.
+    from pandas.io.sql import SQLTable
+
     class SQLTableWithDialectParameters(SQLTable):
         def _create_table_setup(self):
             table = super()._create_table_setup()
@@ -25,3 +28,56 @@ def patch_pandas_io_sqldatabase_with_dialect_parameters(table_kwargs: t.Dict = N
     import pandas.io.sql
 
     pandas.io.sql.SQLTable = SQLTableWithDialectParameters
+
+
+def patch_pandas_sqltable_with_extended_mapping():
+    """
+    Improve pandas' `to_sql` function to respect CrateDB's advanced data types.
+    """
+
+    # It needs to be inlined in order to be composable with `SQLTableWithDialectParameters`.
+    from pandas.io.sql import SQLTable
+
+    class SQLTableWithDtypeMappersForCrateDB(SQLTable):
+        def _get_column_names_and_types(self, dtype_mapper, errors: str = "raise"):
+            column_names_and_types = super()._get_column_names_and_types(dtype_mapper)
+            column_names_and_types_new = []
+            for name, sqlalchemy_type, is_index in column_names_and_types:
+                # TODO: Currently only handles columns. Also make it handle indexes.
+                if is_index:
+                    if errors == "raise":
+                        raise ValueError("pandas indexes not supported yet")
+
+                else:
+                    values = self.frame._get_value(0, name)
+                    if isinstance(values, list):
+                        first_value = values[0]
+                        first_value_type = type(first_value)
+                        # TODO: Use `boltons.remap`.
+                        sqlalchemy_type = ARRAY_TYPE_MAP.get(first_value_type)
+                        if sqlalchemy_type is None:
+                            raise TypeError(f"Data type not supported yet: List[{first_value_type}]")
+
+                column_names_and_types_new.append((name, sqlalchemy_type, is_index))
+
+            return column_names_and_types_new
+
+        def _execute_create(self) -> None:
+            # Prevent `InvalidColumnNameException["_id" conflicts with system column pattern]`.
+            # FIXME: Review, and solve differently.
+            ddl = self.sql_schema()
+            ddl = ddl.replace('"_id" STRING,', "")
+            with self.pd_sql.run_transaction():
+                self.pd_sql.con.exec_driver_sql(ddl)
+
+    # Activate enhancement code.
+    import pandas.io.sql
+
+    pandas.io.sql.SQLTable = SQLTableWithDtypeMappersForCrateDB
+
+
+ARRAY_TYPE_MAP = {
+    str: sa.ARRAY(sa.TEXT),
+    int: sa.ARRAY(sa.INTEGER),
+    float: sa.ARRAY(sa.FLOAT),
+}
