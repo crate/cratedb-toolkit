a89bea255651a8eafed905fc8daed2e7c0a994cc
Andreas Motl <andreas.motl@crate.io>
2024-02-12 13:26:43 +0100

    Testing: Only optionally use the recently added `pytest11` entrypoint
    Corresponding functionality depends on `testcontainers`, which is not
    always installed. It is only available out of the box when installing
    Toolkit using `pip install 'cratedb-toolkit[testing]'`.

--- cratedb_toolkit/testing/pytest.py
+++ cratedb_toolkit/testing/pytest.py
@@ -2,15 +2,26 @@ from typing import Generator
 
 import pytest
 
-from cratedb_toolkit.testing.testcontainers.cratedb import CrateDBTestAdapter
+try:
+    from cratedb_toolkit.testing.testcontainers.cratedb import CrateDBTestAdapter
 
+    @pytest.fixture(scope="session")
+    def cratedb_service() -> Generator[CrateDBTestAdapter, None, None]:
+        """
+        Provide a CrateDB service instance to the test suite.
+        """
+        db = CrateDBTestAdapter()
+        db.start()
+        yield db
+        db.stop()
 
-@pytest.fixture(scope="session")
-def cratedb_service() -> Generator[CrateDBTestAdapter, None, None]:
-    """
-    Provide a CrateDB service instance to the test suite.
-    """
-    db = CrateDBTestAdapter()
-    db.start()
-    yield db
-    db.stop()
+except ModuleNotFoundError as ex:
+    message = str(ex)
+
+    @pytest.fixture(scope="session")
+    def cratedb_service():
+        """
+        An error handling surrogate used when dependencies are not satisfied.
+        In order to use it, invoke `pip install 'cratedb-toolkit[testing]'`.
+        """
+        raise NotImplementedError(message)
