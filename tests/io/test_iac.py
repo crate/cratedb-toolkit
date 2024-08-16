2df4163771384ecb2b7f35501b2f1a8c76ac0db9
Andreas Motl <andreas.motl@crate.io>
2024-08-16 12:48:31 +0200

    Kinesis: Add CI configuration for software tests

--- tests/io/test_iac.py
+++ tests/io/test_iac.py
@@ -1,4 +1,9 @@
 # ruff: noqa: F401
+import pytest
+
+pytestmark = pytest.mark.kinesis
+
+pytest.importorskip("lorrystream", reason="Only works with LorryStream installed")
 
 
 def test_iac_imports():
