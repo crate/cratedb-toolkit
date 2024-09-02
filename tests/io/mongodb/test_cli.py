d2b3e5922af2b80d171639b1041ff3a0c02743ca
Andreas Motl <andreas.motl@crate.io>
2024-09-02 20:26:09 +0200

    MongoDB: Add integration test

--- tests/io/mongodb/test_cli.py
+++ tests/io/mongodb/test_cli.py
@@ -1,5 +1,8 @@
 import os
+from unittest import mock
+from uuid import UUID
 
+import dateutil
 import pytest
 from click.testing import CliRunner
 from pueblo.testing.dataframe import DataFrameFactory
@@ -9,6 +12,7 @@ from tests.conftest import check_sqlalchemy2
 
 pytestmark = pytest.mark.mongodb
 
+bson = pytest.importorskip("bson", reason="Skipping tests because bson is not installed")
 pymongo = pytest.importorskip("pymongo", reason="Skipping tests because pymongo is not installed")
 pytest.importorskip("rich", reason="Skipping tests because rich is not installed")
 
@@ -29,7 +33,26 @@ def test_version():
     assert exitcode == 0
 
 
-def test_mongodb_load_table(caplog, cratedb, mongodb):
+DOCUMENT_IN = {
+    "id": bson.Binary.from_uuid(UUID("d575540f-759d-4653-a4c4-4a9e410f1aa1")),
+    "value": {
+        "name": "foobar",
+        "active": True,
+        "created": dateutil.parser.parse("2020-06-19T15:03:53.727Z"),
+    },
+}
+DOCUMENT_OUT = {
+    "__id": mock.ANY,
+    "id": "d575540f-759d-4653-a4c4-4a9e410f1aa1",
+    "value": {
+        "name": "foobar",
+        "active": True,
+        "created": 1592579033000,
+    },
+}
+
+
+def test_mongodb_load_table_basic(caplog, cratedb, mongodb):
     """
     CLI test: Invoke `ctk load table` for MongoDB.
     """
@@ -59,3 +82,34 @@ def test_mongodb_load_table(caplog, cratedb, mongodb):
     assert cratedb.database.table_exists("testdrive.demo") is True
     assert cratedb.database.refresh_table("testdrive.demo") is True
     assert cratedb.database.count_records("testdrive.demo") == 42
+
+
+def test_mongodb_load_table_real(caplog, cratedb, mongodb):
+    """
+    CLI test: Invoke `ctk load table` for MongoDB.
+    """
+    cratedb_url = f"{cratedb.get_connection_url()}/testdrive/demo"
+    mongodb_url = f"{mongodb.get_connection_url()}/testdrive/demo"
+
+    # Populate source database.
+    client: pymongo.MongoClient = mongodb.get_connection_client()
+    testdrive = client.get_database("testdrive")
+    demo = testdrive.create_collection("demo")
+    demo.insert_many([DOCUMENT_IN])
+
+    # Run transfer command.
+    runner = CliRunner(env={"CRATEDB_SQLALCHEMY_URL": cratedb_url})
+    result = runner.invoke(
+        cli,
+        args=f"load table {mongodb_url}",
+        catch_exceptions=False,
+    )
+    assert result.exit_code == 0
+
+    # Verify data in target database.
+    assert cratedb.database.table_exists("testdrive.demo") is True
+    assert cratedb.database.refresh_table("testdrive.demo") is True
+    assert cratedb.database.count_records("testdrive.demo") == 1
+
+    results = cratedb.database.run_sql("SELECT * FROM testdrive.demo", records=True)
+    assert results[0] == DOCUMENT_OUT
