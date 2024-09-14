8f4e4c6d1f5b9deb80f2ec4f193cfa51ef3ef747
Andreas Motl <andreas.motl@crate.io>
2024-09-15 01:10:11 +0200

    DynamoDB Relay: Refactor "drop stream" action into test fixture

--- tests/io/dynamodb/conftest.py
+++ tests/io/dynamodb/conftest.py
@@ -1,14 +1,24 @@
 import logging
+import time
+import typing
 
+import botocore
 import pytest
 from yarl import URL
 
+from cratedb_toolkit.io.dynamodb.adapter import DynamoDBAdapter
+from cratedb_toolkit.io.kinesis.adapter import KinesisAdapter
 from tests.io.dynamodb.manager import DynamoDBTestManager
 
 logger = logging.getLogger(__name__)
 
 
-# Define databases to be deleted before running each test case.
+# Define streams to be deleted before running each test case.
+RESET_STREAMS = [
+    "demo",
+]
+
+# Define tables to be deleted before running each test case.
 RESET_TABLES = [
     "ProductCatalog",
 ]
@@ -17,11 +27,15 @@ RESET_TABLES = [
 class DynamoDBFixture:
     """
     A little helper wrapping Testcontainer's `LocalStackContainer`.
+
+    TODO: Generalize into `LocalStackFixture`.
     """
 
     def __init__(self):
         self.container = None
         self.url = None
+        self.dynamodb_adapter: typing.Union[DynamoDBAdapter, None] = None
+        self.kinesis_adapter: typing.Union[KinesisAdapter, None] = None
         self.setup()
 
     def setup(self):
@@ -32,17 +46,49 @@ class DynamoDBFixture:
         self.container.with_services("dynamodb", "kinesis")
         self.container.start()
 
+        self.dynamodb_adapter = DynamoDBAdapter(URL(f"{self.get_connection_url_dynamodb()}/?region=us-east-1"))
+        self.kinesis_adapter = KinesisAdapter(
+            URL(f"{self.get_connection_url_kinesis_dynamodb_cdc()}/?region=us-east-1")
+        )
+
     def finalize(self):
         self.container.stop()
 
     def reset(self):
         """
-        Drop all databases used for testing.
+        Reset all resources to provide each test case with a fresh canvas.
         """
-        # FIXME
-        return
-        for database_name in RESET_TABLES:
-            self.client.drop_database(database_name)
+        self.reset_streams()
+        self.reset_tables()
+
+    def reset_streams(self):
+        """
+        Drop all Kinesis streams used for testing.
+        """
+        kinesis_client = self.kinesis_adapter.kinesis_client
+        for stream_name in RESET_STREAMS:
+            try:
+                kinesis_client.delete_stream(StreamName=stream_name)
+            except botocore.exceptions.ClientError as error:
+                if error.response["Error"]["Code"] != "ResourceNotFoundException":
+                    raise
+            waiter = kinesis_client.get_waiter("stream_not_exists")
+            waiter.wait(StreamName=stream_name, WaiterConfig={"Delay": 0.3, "MaxAttempts": 15})
+            time.sleep(0.25)
+
+    def reset_tables(self):
+        """
+        Drop all DynamoDB tables used for testing.
+        """
+        dynamodb_client = self.dynamodb_adapter.dynamodb_client
+        for table_name in RESET_TABLES:
+            try:
+                dynamodb_client.delete_table(TableName=table_name)
+            except botocore.exceptions.ClientError as error:
+                if error.response["Error"]["Code"] != "ResourceNotFoundException":
+                    raise
+            waiter = dynamodb_client.get_waiter("table_not_exists")
+            waiter.wait(TableName=table_name, WaiterConfig={"Delay": 0.3, "MaxAttempts": 15})
 
     def get_connection_url_dynamodb(self):
         url = URL(self.container.get_url())
