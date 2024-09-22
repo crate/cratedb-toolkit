6850bd26c7bfc28ccb24f934891081ba21e0c3f0
Andreas Motl <andreas.motl@crate.io>
2024-09-22 11:50:10 +0200

    Table Loader: Improve status reporting + error logging in BulkProcessor

--- cratedb_toolkit/util/common.py
+++ cratedb_toolkit/util/common.py
@@ -1,12 +1,15 @@
 # Copyright (c) 2023, Crate.io Inc.
 # Distributed under the terms of the AGPLv3 license, see LICENSE.
 import logging
+import os
 
 import colorlog
 from colorlog.escape_codes import escape_codes
 
+from cratedb_toolkit.util.data import asbool
 
-def setup_logging(level=logging.INFO, verbose: bool = False, width: int = 36):
+
+def setup_logging(level=logging.INFO, verbose: bool = False, debug: bool = False, width: int = 36):
     reset = escape_codes["reset"]
     log_format = f"%(asctime)-15s [%(name)-{width}s] %(log_color)s%(levelname)-8s:{reset} %(message)s"
 
@@ -15,13 +18,24 @@ def setup_logging(level=logging.INFO, verbose: bool = False, width: int = 36):
 
     logging.basicConfig(format=log_format, level=level, handlers=[handler])
 
-    # Enable SQLAlchemy logging.
-    if verbose:
-        logging.getLogger("sqlalchemy").setLevel(level)
-
     logging.getLogger("crate.client").setLevel(level)
     logging.getLogger("sqlalchemy_cratedb").setLevel(level)
     logging.getLogger("urllib3.connectionpool").setLevel(level)
+
+    # Enable SQLAlchemy logging.
+    if verbose:
+        logging.getLogger("cratedb_toolkit").setLevel(logging.DEBUG)
+
+    if debug:
+        # Optionally tame SQLAlchemy and PyMongo.
+        if asbool(os.environ.get("DEBUG_SQLALCHEMY")):
+            logging.getLogger("sqlalchemy").setLevel(level)
+        else:
+            logging.getLogger("sqlalchemy").setLevel(logging.INFO)
+        if asbool(os.environ.get("DEBUG_PYMONGO")):
+            logging.getLogger("pymongo").setLevel(level)
+        else:
+            logging.getLogger("pymongo").setLevel(logging.INFO)
 
     # logging.getLogger("docker.auth").setLevel(logging.INFO)  # noqa: ERA001
 
