e7fbdfd99320493f02da8adaa0fdb3054df3efec
Kenneth Geisshirt <k@zigzak.net>
2026-07-03 12:45:05 +0200

    Change to expected error messages

--- tests/cluster/test_model.py
+++ tests/cluster/test_model.py
@@ -7,4 +7,4 @@ from cratedb_toolkit.exception import CroudException
 def test_cluster_info_managed_unauthorized():
     with pytest.raises(CroudException) as ex:
         ClusterInformation.from_id_or_name(cluster_id="foo", cluster_name="bar")
-    assert ex.match("401 - Unauthorized")
+    assert ex.match("{'error': {'reason': 'Unauthorized', 'message': 'Missing access_token'}, 'status': 401}")
